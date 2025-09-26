ffe1e5d81841be066b052ca57116f63455a90b2a
uzaydurdu <uzaydurdu1999@gmail.com>
2025-09-26 14:56:48 +0200

    Created stable branch for version 6.0.3 to be compatible with kamailio apt packages from focal60 source

--- src/core/parser/digest/digest_parser.c
+++ src/core/parser/digest/digest_parser.c
@@ -52,6 +52,12 @@
 #define ALG_MD5SESS_STR "MD5-sess"
 #define ALG_MD5SESS_STR_LEN 8
 
+#define ALG_SHA256_STR "SHA-256"
+#define ALG_SHA256_STR_LEN 7
+
+#define ALG_SHA256SESS_STR "SHA-256-sess"
+#define ALG_SHA256SESS_STR_LEN 12
+
 
 /*
  * Parse quoted string in a parameter body
@@ -282,6 +288,12 @@ static inline void parse_algorithm(struct algorithm *_a)
 	} else if((s.len == ALG_MD5SESS_STR_LEN)
 			  && !strncasecmp(s.s, ALG_MD5SESS_STR, ALG_MD5SESS_STR_LEN)) {
 		_a->alg_parsed = ALG_MD5SESS;
+	} else if((s.len == ALG_SHA256_STR_LEN)
+		   && !strncasecmp(s.s, ALG_SHA256_STR, ALG_SHA256_STR_LEN)) {
+			_a->alg_parsed = ALG_SHA256;
+	} else if((s.len == ALG_SHA256SESS_STR_LEN)
+		   && !strncasecmp(s.s, ALG_SHA256SESS_STR, ALG_SHA256SESS_STR_LEN)) {
+			_a->alg_parsed = ALG_SHA256SESS;
 	} else {
 		_a->alg_parsed = ALG_OTHER;
 	}
